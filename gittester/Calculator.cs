ab010df571c0cdeb2ae90df6470f31a56f9a7c45
delli <delli84@hotmail.com>
2014-02-03 11:23:14 +0100

    added calculator class

---
+++ gittester/Calculator.cs
@@ -0,0 +1,24 @@
+﻿namespace gittester
+{
+    public class Calculator
+    {
+        private int _cvalue;
+
+        public Calculator(int cvalue)
+        {
+            this._cvalue = cvalue;
+        }
+
+        public void AddNumber(int val)
+        {
+
+            _cvalue = Cvalue + val;
+        }
+
+        public int Cvalue
+        {
+            get { return _cvalue; }
+            set { _cvalue = value; }
+        }
+    }
+}
